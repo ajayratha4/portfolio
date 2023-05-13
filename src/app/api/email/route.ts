446aabf19d56135f2d486a04f04b5dcc9323e541
ajayratha4 <ajayratha4@gmail.com>
2023-05-13 15:37:01 +0530

    Fixed email

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -10,8 +10,8 @@ import { sendEmail } from "./sendEmail";
 export async function POST(req: Request) {
   const body = await req.json();
   const data = await sendEmail({
-    subject: `Portfolio Ajay Ratha ${body.subject}`,
-    text: `Hello from ${body.email} ==> ${body.message}`,
+    subject: `Portfolio Ajay Ratha subject=> ${body.subject}`,
+    text: `Name =>${body.name}|| from  Email => ${body.email}|| Message ==> ${body.message} ||`,
   });
   return NextResponse.json({ data });
 }
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,11 +1,46 @@
-import React from "react";
-import Landing from "../Landing";
-import { rampartOne } from "@/fonts/fonts";
+"use client";
+import React, { useState } from "react";
 import TagWrapper from "../common/TagWrapper";
 import Button from "../common/Button";
 import HeaderTag from "../common/HeaderTag";
 
+const initialFormData = { name: "", email: "", subject: "", message: "" };
+
 const Contact = () => {
+  const [isLoading, setLoading] = useState(false);
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleOnSubmit = () => {
+    const { name, email, subject, message } = formData;
+    if (name && email && subject) {
+      setLoading(true);
+      fetch(`http://localhost:3001/api/email`, {
+        method: "POST",
+        body: JSON.stringify({
+          subject,
+          email,
+          message,
+          name,
+        }),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setFormData(initialFormData);
+        })
+        .catch((err) => console.error(err))
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  };
+
+  const handleOnChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
   return (
     <div className="w-full md:w-3/4 flex flex-col gap-3">
       <HeaderTag>
@@ -24,14 +59,16 @@ const Contact = () => {
         <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
             <label
-              htmlFor="first_name"
+              htmlFor="name"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Name
             </label>
             <input
+              value={formData.name}
+              onChange={handleOnChange}
               type="text"
-              id="first_name"
+              id="name"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="John"
               required
@@ -39,12 +76,14 @@ const Contact = () => {
           </div>
           <div>
             <label
-              htmlFor="first_name"
+              htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Email
             </label>
             <input
+              value={formData.email}
+              onChange={handleOnChange}
               type="email"
               id="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -55,12 +94,14 @@ const Contact = () => {
         </div>
         <div className="mb-6">
           <label
-            htmlFor="email"
+            htmlFor="subject"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Subject
           </label>
           <input
+            value={formData.subject}
+            onChange={handleOnChange}
             type="subject"
             id="subject"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -70,21 +111,23 @@ const Contact = () => {
         </div>
         <div className="mb-6">
           <label
-            htmlFor="email"
+            htmlFor="message"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Subject
+            Message
           </label>
           <textarea
+            value={formData.message}
+            onChange={handleOnChange}
             rows={3}
             cols={50}
             id="message"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="message."
+            placeholder="message..."
           />
         </div>
         <div className="flex justify-center">
-          <Button>Send Message!</Button>
+          <Button onClick={handleOnSubmit}>Send Message!</Button>
         </div>
       </TagWrapper>
     </div>
