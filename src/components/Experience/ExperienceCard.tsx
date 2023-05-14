d0af0914b421e48d1cb9fb8c417a53d3674c5f40
ajayratha4 <ajayratha4@gmail.com>
2023-05-15 02:25:52 +0530

    Fixed

--- src/components/Experience/ExperienceCard.tsx
+++ src/components/Experience/ExperienceCard.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const ExperienceCard = () => {
-  return <div>E-Commerce Website</div>;
+type Props = { title: string; details: string };
+
+const ExperienceCard = ({ title, details }: Props) => {
+  return (
+    <div>
+      <div className="flex flex-col items-center border  rounded-lg shadow md:flex-row md:max-w-xl">
+        {/* <img
+          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src="/docs/images/blog/image-4.jpg"
+          alt=""
+        /> */}
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="text-lg md:text-2xl">{title}</h5>
+          <p>{details}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExperienceCard;
--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -8,8 +8,27 @@ const Experience = () => {
       <HeaderTag>
         <span>Portfolio</span>
       </HeaderTag>
-      <div>
-        <ExperienceCard /> <ExperienceCard /> <ExperienceCard />
+      <div className="flex flex-col gap-3">
+        <ExperienceCard
+          title="Music Application"
+          details="music streaming service where users can
+listen to their favorite podcasts or music and share with
+anyone."
+        />
+        <ExperienceCard
+          title="E-Commerce Website"
+          details="a transportation management service where a
+user can rent a 2PTruck, 4PTruck and 6PTruck and The
+vendor can track the data of the user."
+        />
+        <ExperienceCard
+          title="AI-based Website "
+          details="Transforming enterprise security operations
+with computer vision intelligence to prevent security
+incidents from occurring in the first place. User can set a
+costume alert, for example if a person is detected in a
+certain area, the alert will come to the user etc..."
+        />
       </div>
     </div>
   );
